2e54af64740da00bf0a2f244b5e96c8e64df8109
Yevgeniy Bazilov <jenya12d@gmail.com>
2023-10-18 17:15:44 +0500

    leson2-task5

---
+++ lesson-02/05.js
@@ -0,0 +1,10 @@
+let passport = {
+  name: "Petr",
+  surname: "Petrov",
+};
+
+let passportClone = { ...passport };
+passportClone.name = "Ivan";
+
+console.log(passport);
+console.log(passportClone);

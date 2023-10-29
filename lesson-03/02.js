3738100cddd8a7a71b4676ba0ae759005256d53d
Bazilov Yevgeniy <jenya12d@gmail.com>
2023-10-29 16:03:32 +0500

    deleted consol.log

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,5 +1,4 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
-console.log(words.indexOf("апельсин"))
 if (words.indexOf("апельсин") >= 0) {
   console.log("Ура! нашел");
 } else console.log("Придется поискать в другом магазине…");

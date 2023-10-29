344bd8b3d8057898ece5972454640d8c093b8cd3
Bazilov Yevgeniy <jenya12d@gmail.com>
2023-10-29 15:54:31 +0500

    lesson-03-task-02

---
+++ lesson-03/02.js
@@ -0,0 +1,5 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+console.log(words.indexOf("апельсин"))
+if (words.indexOf("апельсин") >= 0) {
+  console.log("Ура! нашел");
+} else console.log("Придется поискать в другом магазине…");
